6ec0b9d5adf08ea64d3faaeff68123444ac12b13
aurel fontaine <ajfame11@gmail.com>
2020-03-25 19:56:14 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def #parrot("name", phrase = "Squawk!")
+def #parrot(phrase)
+    phrase = "Squack!"
     puts "#{phrase}"
     return "#{phrase}"
 end
